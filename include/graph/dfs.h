b67afee469c236c00fd55f6587cce19393660819
Jackson <jackson.harmon12@gmail.com>
2023-12-02 10:29:43 +0100

    made method defintions to be outside of class

--- include/graph/dfs.h
+++ include/graph/dfs.h
@@ -7,21 +7,29 @@ class DFS {
   BinaryNode<T> root;
 
 public:
-  DFS(BinaryNode<T>& root): root(root) {
-  }
-
-  void traverseAsVector(std::vector<T>& v) {
-    traverseAsVectorTailRecursion(v, &this->root);
-  }
-
-  void traverseAsVectorTailRecursion(std::vector<T>& v, BinaryNode<T>* node) {
-    if (node == nullptr) {
-      return;
-    }
-
-    v.push_back(node->getValue());
-
-    traverseAsVectorTailRecursion(v, node->getLeft());
-    traverseAsVectorTailRecursion(v, node->getRight());
-  }
+  DFS(BinaryNode<T>& root);
+  void traverseAsVector(std::vector<T>& v);
+  void traverseAsVectorTailRecursion(std::vector<T>& v, BinaryNode<T>* node);
 };
+
+
+template<typename T>
+DFS<T>::DFS(BinaryNode<T>& root): root(root) {
+}
+
+template<typename T>
+void DFS<T>::traverseAsVector(std::vector<T>& v) {
+  traverseAsVectorTailRecursion(v, &this->root);
+}
+
+template<typename T>
+void DFS<T>::traverseAsVectorTailRecursion(std::vector<T>& v, BinaryNode<T>* node) {
+  if (node == nullptr) {
+    return;
+  }
+
+  v.push_back(node->getValue());
+
+  traverseAsVectorTailRecursion(v, node->getLeft());
+  traverseAsVectorTailRecursion(v, node->getRight());
+}
